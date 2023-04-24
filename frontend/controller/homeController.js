0e5bdf5d9d1d30dc56aaa7e9be6f29ac4b8c5309
DanJacker <125642292+DanJacker@users.noreply.github.com>
2023-04-24 13:08:12 +0700

    2

--- frontend/controller/homeController.js
+++ frontend/controller/homeController.js
@@ -15,8 +15,12 @@ let getAddban = async (req, res) => {
     let { vitri, nguoi, trangthai } = req.body;
     await pool.execute('insert into ban(vitri, nguoi, trangthai) values (?, ?, ?)', [vitri, nguoi, trangthai]);
     return res.redirect('/ql_ban.html')
-
+}
+let getAddphieugiamgia = async (req, res) => {
+    let { magg, giam, toida, daystart, dayfinish, luong } = req.body;
+    await pool.execute('insert into ban(magg, giam, toida, daystart, dayfinish, luong) values (?, ?, ?)', [magg, giam, toida, daystart, dayfinish, luong]);
+    return res.redirect('/ql_phieugiamgia.html')
 }
 module.exports = {
-    getHomepage, getAddnhanvien, getAddban
+    getHomepage, getAddnhanvien, getAddban, getAddphieugiamgia
 }
--- frontend/route/web.js
+++ frontend/route/web.js
@@ -8,6 +8,7 @@ const initWebRoute = (app) => {
     router.get('/', homeController.getHomepage);
     router.post('/add_nhanvien.html', homeController.getAddnhanvien);
     router.post('/ql_ban.html', homeController.getAddban);
+    router.post('/ql_phieugiamgia.html', homeController.getAddphieugiamgia);
 
     return app.use('/', router)
 }
